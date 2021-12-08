0d534ae49a11ba8ea63263c917986af5f55ee9b6
Anita Deszczynska <anitadeszczynska@gmail.com>
2021-12-08 12:13:34 +0100

    Remove class active from all articles

--- js/script.js
+++ js/script.js
@@ -28,5 +28,20 @@
     link.addEventListener('click', titleClickHandler);
   }
 
+    /* remove class 'active' from all article links */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }   
+
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post-active .title')
+
+    for (let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
+
 
 }
