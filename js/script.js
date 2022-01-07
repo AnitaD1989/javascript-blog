928fb4fb1a27c16d28eb1b1cb257bbb0146aaa7d
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-01-07 17:33:47 +0100

    Add calcuteTagsParms function

--- js/script.js
+++ js/script.js
@@ -66,6 +66,32 @@ const titleClickHandler = function(event){
   generateTitleLinks();
 }
 
+function calculateTagsParams(tags){
+  const params = { max : 0, min : 999999 };
+
+  for(let tag in tags){
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+
+    if (tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+    console.log(`${tag} 'is used' ${tags[tag]} 'times'`);
+  }
+  return params;
+}
+
+function calculateTagClass(count, params){
+
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor( percentage * (opts.cloudClassCount - 1) + 1 );
+
+  return opts.cloudClassPrefix + classNumber;
+}
+
 function generateTags() {
 
   /* [NEW] create a new variable allTags with an empty object */
@@ -120,6 +146,9 @@ function generateTags() {
 
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector(optTagsListSelector);
+
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams)
 
   /* [NEW] create variable for all links HTML code */
   let allTagsHTML = '';
